5a2baede704e6e68346550936c5f521f01c90939
Roylitoberes <royoberes333@gmail.com>
2026-04-30 07:18:06 -0700

    Add files via upload

---
+++ php/test.php
@@ -0,0 +1,7 @@
+<?php
+// test.php
+
+echo "<h1>🎉 Your XAMPP Virtual Host is working!</h1>";
+echo "<p>Current directory: " . __DIR__ . "</p>";
+echo "<p>Server software: " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
+?>
